6cafba4d7c2e406bd38925fb8cbc8c21bb031223
ubuntu030 <ubuntu030@gmail.com>
2021-03-30 21:06:19 +0800

    add table-info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableInfoComponent } from './table-info/table-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableInfoComponent
   ],
   imports: [
     BrowserModule,
