ee1889421e7c420ce2942491e99c7ac3cb33ba05
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-03-31 09:40:04 +0800

    update bootstrap module

--- src/app/app-bootstrap/app-bootstrap.module.ts
+++ src/app/app-bootstrap/app-bootstrap.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDropdownModule, BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { AlertModule, AlertConfig } from 'ngx-bootstrap/alert';
+import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
+import {
+  BsDatepickerModule,
+  BsDatepickerConfig,
+} from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    BsDropdownModule.forRoot(),
+    BsDropdownModule,
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
-  ]
+    AccordionModule,
+    AlertModule,
+    ButtonsModule,
+    CarouselModule,
+    CollapseModule,
+    BsDatepickerModule.forRoot(),
+    BsDropdownModule,
+    ModalModule,
+  ],
+  providers: [
+    AlertConfig,
+    BsDatepickerConfig,
+    BsDropdownConfig,
+    BsModalService,
+  ],
 })
-export class AppBootstrapModule { }
+export class AppBootstrapModule {}
