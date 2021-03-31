ca84b718fc01b09b69e18cc0a30eb5ac884e2c42
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-03-31 09:58:00 +0800

    add bootstrap modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppBootstrapModule } from "./app-bootstrap/app-bootstrap.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableInfoComponent } from './table-info/table-info.component';
+import { DownloadApplicationComponent } from './download-application/download-application.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableInfoComponent
+    TableInfoComponent,
+    DownloadApplicationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/download-application/download-application.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-download-application',
+  templateUrl: './download-application.component.html',
+  styleUrls: ['./download-application.component.scss'],
+})
+export class DownloadApplicationComponent implements OnInit {
+  modalRef!: BsModalRef;
+
+  constructor(private modalService: BsModalService) {}
+
+  ngOnInit(): void {}
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+}
