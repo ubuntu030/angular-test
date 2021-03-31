d6bb7dca5f3ad5ac4cc38975179297e0d40c4e7f
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-03-31 15:28:04 +0800

    add routing for component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DownloadApplicationComponent } from "./download-application/download-application.component";
+import { DownloadApprovalComponent } from "./download-approval/download-approval.component";
+
+const routes: Routes = [
+  {path:'', redirectTo: '/download', pathMatch: 'full'},
+  { path: 'download', component: DownloadApplicationComponent },
+  { path: 'approval', component: DownloadApprovalComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
