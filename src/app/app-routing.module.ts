7eed99db99ef1518cd7e120ea848132517b64a72
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-04-01 10:20:19 +0800

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 // import { DownloadApplicationComponent } from "./download-application/download-application.component";
 import { DownloadApprovalComponent } from "./download-approval/download-approval.component";
 import { TableInfoComponent } from "./table-info/table-info.component";
+import { MaintainComponent } from "./maintain/maintain.component";
 
 const routes: Routes = [
   {path:'', redirectTo: '/download', pathMatch: 'full'},
   { path: 'download', component: TableInfoComponent },
   { path: 'approval', component: DownloadApprovalComponent },
+  { path: 'maintain', component: MaintainComponent },
 ];
 
 @NgModule({
