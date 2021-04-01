5f7eb667565f497c4ec5f64e920e9c1554b3f822
DESKTOP-PJOOH01\Max <ubuntu030@gmail.com>
2021-04-01 10:22:19 +0800

    apply bootstrap modal logic

--- src/app/maintain/maintain.component.ts
+++ src/app/maintain/maintain.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-maintain',
   templateUrl: './maintain.component.html',
-  styleUrls: ['./maintain.component.scss']
+  styleUrls: ['./maintain.component.scss'],
 })
 export class MaintainComponent implements OnInit {
+  modalRef!: BsModalRef;
 
-  constructor() { }
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit(): void {
+    console.log('maintain init');
   }
 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(
+      template,
+      Object.assign({}, { class: 'modal-lg' })
+    );
+  }
 }
